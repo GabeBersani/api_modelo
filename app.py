ec84f21e11d749b64b94cae3948c0ec4d68a37ef
GabeBersani <gabi-fernanda1@outlook.com>
2025-03-20 09:48:54 -0300

    api

--- app.py
+++ app.py
@@ -4,33 +4,52 @@ app = Flask(__name__)
 # def index(informacao_texto):
 #   return f'Hello, World, {informacao_texto} !'
 
-@app.route('/soma/<int:num1>+<float:num2>')
+@app.route('/soma/<num1>+<num2>')
 def soma (num1, num2):
-    return f'{num1} + {num2} = {num1 + num2}'
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+        return str(num1 + num2)
+    except ValueError:
+        return "Apenas numeros inteiros"
 
-@app.route('/sub/<int:num1>-<int:num2>')
+@app.route('/sub/<num1>-<num2>')
 def subtracao (num1, num2):
-    return f'{num1} - {num2} = {num1 - num2}'
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+        return str(num1 - num2)
+    except ValueError:
+        return "Apenas numeros inteiros"
 
-@app.route('/multiplicacao/<int:num1>*<int:num2>')
+@app.route('/multiplicacao/<num1>*<num2>')
 def multiplicacao (num1, num2):
-    return f'{num1} * {num2} = {num1 * num2}'
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+        return str(num1 * num2)
+    except ValueError:
+        return "Apenas numeros inteiros"
 
-@app.route('/divisao/<int:num1>/<int:num2>')
+@app.route('/divisao/<num1>/<num2>')
 def divisao (num1, num2):
-    return f'{num1} / {num2} = {num1 / num2}'
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+        return str(num1 / num2)
+    except ValueError:
+        return "Apenas numeros inteiros"
 
 @app.route('/verficar/<int:num1>')
 def verficar (num1):
-    if num1 % 2 == 0:
-        return f'O número {num1} é par'
-    elif num1 % 2 == 1:
-        return f'O número {num1}, é impar'
-    else:
-        print("algo deu errado")
-
-
-
+    try:
+        num1 = int(num1)
+        if num1 % 2 == 0:
+            return f'O número {num1} é par'
+        elif num1 % 2 == 1:
+            return f'O número {num1}, é impar'
+    except ValueError:
+        return "Apenas numeros inteiros"
 
 
 if __name__ == '__main__':
